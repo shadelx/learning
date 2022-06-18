49f0a18662058509f56cf7d94dae85347d659c92
shadelx <shadelx.07@gmail.com>
2022-06-17 22:20:44 -0500

    add new assembly folder

---
+++ assembly/nasm/README.md
@@ -0,0 +1,6 @@
+# simple assembly hello world code with nasm
+
+commands
+
+1. nasm -felf64 app.asm 
+2. ld app.o -o app
---
+++ assembly/nasm/app.asm
@@ -0,0 +1,19 @@
+;define variables in the data section
+section .data
+    hello: db "hello vic", 10    ;string to print
+    helloLen: equ $-hello        ;length of string
+
+section .text
+    global _start               ;entry point for linker
+
+    _start:
+        mov rax,1               ;sys_write
+        mov rdi,1               ;stdout
+        mov rsi,hello           ;message to write
+        mov rdx,helloLen        ;message length
+        syscall                 ;call kernel
+
+        ;end program
+        mov rax,60              ;sys_exit
+        mov rdi,0               ;error code 0 (success)
+        syscall
